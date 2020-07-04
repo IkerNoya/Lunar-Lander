c372094d30f8e988a3f9bd4147cac9aaca4f5e51
Iker <ikernoya4@gmail.com>
2020-07-04 14:57:17 -0300

    Created player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float force;
+    public Rigidbody2D thrusterForce;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            //transform.position +=  * speed;
+        }
+    }
+}
