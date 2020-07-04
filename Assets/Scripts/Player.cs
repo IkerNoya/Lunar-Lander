aecb63f5936e3bb629ac8c4aa3714f6072bedc6b
Iker <ikernoya4@gmail.com>
2020-07-04 15:24:21 -0300

    Changed gravity scale to simulate the moon's gravity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,40 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float force;
-    public Rigidbody2D thrusterForce;
-    private void Update()
+    Rigidbody2D thruster;
+    Vector2 thrusterForce;
+    bool isThrusterActivated = false;
+    float moonGravity = 1.62f;
+    float earthGravity = 9.8f;
+    float relativeMoonGravity;
+    float originalGravityScale = 1;
+    float newGravityScale;
+    Vector2 gravity;
+    void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        thruster = GetComponent<Rigidbody2D>();
+        gravity = new Vector2(0, moonGravity);
+        newGravityScale = (moonGravity * originalGravityScale) / earthGravity; //rule of three
+        thruster.gravityScale = newGravityScale; // simulation of moon gravity
+
+    }
+    void Update()
+    {
+        thrusterForce = new Vector2(0, force);
+        if (Input.GetKey(KeyCode.Space))
         {
-            
-            //transform.position +=  * speed;
+            isThrusterActivated = true;
+        }
+        else
+        {
+            isThrusterActivated = false;
+        }
+    }
+    void FixedUpdate()
+    {
+        if(isThrusterActivated)
+        {
+            thruster.AddForce(thrusterForce);
         }
     }
 }
