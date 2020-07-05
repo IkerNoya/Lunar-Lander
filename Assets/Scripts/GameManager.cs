280af82179e7f3338fb9dbca333a01f483960c3a
Iker <ikernoya4@gmail.com>
2020-07-04 21:03:16 -0300

    Created GameManager singleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+   public static GameManager Get()
+    {
+        return instance;
+    }
+   void Awake()
+    {
+        if(instance!=null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
